ac0529ce6d6dc749d0fa6db229e1fe00339d1270
Jason Park <jjpark987@gmail.com>
2022-11-09 18:53:50 -0800

    commit tree e77b887d1f48d9e2c25d756424e0d5352c708ecb

--- main.js
+++ main.js
@@ -3,9 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeButtons = document.body.getElementsByClassName('like-glyph')
+for(let button of likeButtons) {
+  button.addEventListener('click', handleClick)
+}
 
+const modal = document.getElementById('modal')
 
+function handleClick(event) {
+  mimicServerCall()
+  .then(response => {
+    if(response === 'Pretend remote server notified of action!') {
+      changeHeart(event)
+    }
+  })
+  .catch(displayError)
+}
 
+function changeHeart(event) {
+  if(event.target.textContent === EMPTY_HEART) {
+    event.target.innerText = FULL_HEART
+    event.target.className = 'activated-heart'
+  } else {
+    event.target.innerText = EMPTY_HEART
+    event.target.className = ''
+  }
+}
+
+function displayError() {
+  modal.className = ''
+  window.setTimeout(() => modal.className = 'hidden', 3000)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
